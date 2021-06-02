5ce8f8152f3b066bebd7bc94fbae5e15965948aa
Mohit1510 <70361856+Mohit1510@users.noreply.github.com>
2021-06-02 21:56:14 +0530

    setup test database

---
+++ src/playground/promises.js
@@ -0,0 +1,17 @@
+// const promise= new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         // resolve({
+//         //     name:'Andrew',
+//         //     age:34
+//         // })
+//         reject('your data is rejected');
+//     }, 1500)
+// });
+// console.log('before');
+// promise.then((data)=>{
+//      console.log(`1 `,data);
+// }).catch((error)=>{
+//     console.log('error: ', error)
+// })
+
+// console.log('after');
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,10 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import database from '../../firebase/firebase'
+
+const createMockStore = configureMockStore([thunk])
 
 test('should setup remove expense object', () => {
     const action = removeExpense({ id: '123abcd' })
@@ -20,38 +26,80 @@ test('should setup edit expense object', () => {
 })
 
 test('with provided values at add expense', () => {
-    const action = addExpense({
-        description: 'xyz',
-        note: 'abc',
-        createdAt: '1200',
-        amount: 1500
+    
+    const action = addExpense(expenses[2])
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: expenses[2]
     });
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            description: 'xyz',
-            note: 'abc',
-            createdAt: '1200',
-            id:expect.any(String),
-            amount: 1500
-        }
+});
 
-    })
-})
-test('with default values at add expense', () => {
-    const action = addExpense();
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            description: '',
-            note: '',
-            createdAt: 0,
-            id:expect.any(String),
-            amount: 0
-        }
+test('Should add expenses to database and store', (done)=>{
+    const store = createMockStore({});
+    const expenseData={
+        description: 'water bill',
+        amount: 3000,
+        note:'this is really tough',
+        createdAt: 12000000000
+    };
+    store.dispatch(startAddExpense(expenseData)).then(()=>{
+       const actions= store.getActions();
+       expect(actions[0]).toEqual({
+           type: 'ADD_EXPENSE',
+           expense: {
+               id: expect.any(String),
+               ...expenseData
+           }
+       })
 
-    })
+       return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+    }).then((snapshot)=>{
+        expect(snapshot.val()).toEqual(expenseData);
+        done();
+    });
 })
+
+
+
+
+test('Should add expenses with defaults to database and store', (done)=>{
+    const store = createMockStore({});
+    const expenseData={
+        description: '',
+        amount: 0,
+        note:'',
+        createdAt: 0
+    };
+    store.dispatch(startAddExpense(expenseData)).then(()=>{
+       const actions= store.getActions();
+       expect(actions[0]).toEqual({
+           type: 'ADD_EXPENSE',
+           expense: {
+               id: expect.any(String),
+               ...expenseData
+           }
+       })
+
+       return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+    }).then((snapshot)=>{
+        expect(snapshot.val()).toEqual(expenseData);
+        done();
+    });
+})
+// test('with default values at add expense', () => {
+//     const action = addExpense();
+//     expect(action).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             description: '',
+//             note: '',
+//             createdAt: 0,
+//             id:expect.any(String),
+//             amount: 0
+//         }
+
+//     })
+// })
 // test('with default values at add expense', ()=>{
 
 // })
